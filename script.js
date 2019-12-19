e34a2c78521ba113249a3edb88c04908092e1867
Nick Algarra <algarran24@gmail.com>
2019-12-18 22:06:18 -0600

    added variables to limit characters pulled from string

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
-
-// var specialCharacters = ["!", "#", "@", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "?", "/", "<", ">"]
-
-// var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
 // var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
 //  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-// var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+//  var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
 //  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+//  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+// var specialCharacters = ["!", "#", "@", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "?", "/", "<", ">"]
+
+// 
+// 
+// 
+var passwordLength = prompt("How long do you want your password to be?");
+
+if(passwordLength < 8 || passwordLength > 128 ){
+    alert("Not a valid length put select a number between 8 and 128 ****!");
+    }else{
+        var upperCase = confirm("Use upper case letters?");
+        var lowerCase = confirm("Use lower case letters?");
+        var numbers = confirm("Use numbers?");
+        var specialCharacters = confirm("Use special characters?");
+}
+
+let avail=""
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
 
 
-//  var specialCharacters = confirm('Would you like to use special characters?');
-//  var numbers = confirm('Would you like to use numbers?');
-//  var upperCharacters = confirm('Would you like to use uppercase letters');
-//  var lowerCharacters = confirm('Would you like to use lowercase letters');
+if(upperCase){
+    avail += values.substring(0,26);
+}
+
+if(lowerCase){
+    avail += values.substring(26,26);
+}
+ 
+if(numbers){
+    avail += values.substring(52,10);
+}
+
+if(specialCharacters){
+    avail += values.substring(62,10);
+}
 
 
-var passwordLength = prompt("How long do you want your password to be?")
-
-passwordLength.parseInt();
 
 function generate(){
 
 
-    var complexity = passwordLength;
-
     //possible password values
     let password = "";
 
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+<>?";
+    // while(password.length <= length){
+    //     password += avail.charAt(Math.florr(Math.random()* avail.length));
+    // }
 
     
 
-    //create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    // //create for loop to choose password characters
+    for(var i = 1; i <= passwordLength; i++){
+        password = password + avail.charAt(Math.floor(Math.random() * avail.length -1));
     }
 
     //add password to texbox/display area
     document.getElementById("password").value = password;
 
 }
-
-
 
 
 //function to copy password to clipboard
