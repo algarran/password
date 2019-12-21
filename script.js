491e999a7a3462336787bda80b93944c6c2c4c20
Nick Algarra <algarran24@gmail.com>
2019-12-21 10:13:01 -0600

    fixed substring numbers to accurately pull correct parts of the string to generate a password

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 var passwordLength = prompt("How long do you want your password to be?");
 
 if(passwordLength < 8 || passwordLength > 128 ){
-    alert("Not a valid length put select a number between 8 and 128 ****!");
+    alert("Not a valid length put select a number between 8 and 128!");
     }else{
         var upperCase = confirm("Use upper case letters?");
         var lowerCase = confirm("Use lower case letters?");
@@ -31,15 +31,17 @@ if(upperCase){
 }
 
 if(lowerCase){
-    avail += values.substring(26,26);
+    avail += values.substring(26,52);
 }
  
 if(numbers){
-    avail += values.substring(52,10);
+    avail += values.substring(52,62);
 }
 
 if(specialCharacters){
-    avail += values.substring(62,10);
+    console.log("here")
+    avail += values.substring(62);
+    console.log(avail)
 }
 
 
@@ -58,7 +60,7 @@ function generate(){
 
     // //create for loop to choose password characters
     for(var i = 1; i <= passwordLength; i++){
-        password = password + avail.charAt(Math.floor(Math.random() * avail.length -1));
+        password = password + avail.charAt(Math.floor(Math.random() * (avail.length)));
     }
 
     //add password to texbox/display area
